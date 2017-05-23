bb1457318e1c4dbf60839376f939302787b5c783
yusufkocak1 <yusufkocak.53@yandex.com>
2017-05-23 22:50:55 +0300

    mongo get set adapter create

---
+++ app/src/main/java/com/yube/caly/mongoAdapter/getDataAdapter.java
@@ -0,0 +1,26 @@
+package com.yube.caly.mongoAdapter;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by yusuf on 23.05.2017.
+ */
+
+public class getDataAdapter extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+    }
+}
---
+++ app/src/main/java/com/yube/caly/mongoAdapter/setDataAdapter.java
@@ -0,0 +1,10 @@
+package com.yube.caly.mongoAdapter;
+
+/**
+ * Created by yusuf on 23.05.2017.
+ */
+
+public class setDataAdapter {
+
+
+}
